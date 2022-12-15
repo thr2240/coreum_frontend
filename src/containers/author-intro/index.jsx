0d75ceeacc9778836cff17cc5a6626d8c5e00541
thr2240 <thomasr2240@gmail.com>
2022-12-16 00:59:35 +0300

    change padding

--- src/containers/author-intro/index.jsx
+++ src/containers/author-intro/index.jsx
@@ -16,7 +16,7 @@ const AuthorIntroArea = ({ className, space, data }) => {
                 show={isShareModalOpen}
                 handleModal={shareModalHandler}
             />
-            <div className="rn-author-bg-area position-relative ptb--100 mb--10">
+            <div className="rn-author-bg-area position-relative ptb--75 mb--10">
                 <Image
                     src="/images/banner/banner.png"
                     alt="Slider BG"
